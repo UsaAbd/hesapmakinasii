ca5bbf478846fd32f4e6c2dc70544ff098738673
YusufGavga <36437477+YusufGavga@users.noreply.github.com>
2018-02-14 14:09:00 +0300

    Update Form1.cs
    işlemler tanımlanıp buton işlevleri eklendi

--- WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/Form1.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,19 +12,57 @@ namespace WindowsFormsApplication2
 {
     public partial class Form1 : Form
     {
+        public string Islem = String.Empty;
+        public double sayi1 = 0;
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void ekle(object sender, EventArgs e)
         {
-
+             textBox1.Text += (sender as Button).Text;
         }
 
-        private void button7_Click(object sender, EventArgs e)
+        private void islem(object sender, EventArgs e)
         {
+            Islem = (sender as Button).Text;
+            sayi1 = Convert.ToDouble(textBox1.Text);
+            textBox1.Text = "0";
+        }
 
+        private void buttonAC_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+            Islem = String.Empty;
+        }
+
+        private void buttonEsittir_Click(object sender, EventArgs e)
+        {
+            double sayi2 = Convert.ToDouble(textBox1.Text);
+
+            double sonuc = 0;
+
+            switch (Islem)
+            {
+                case "+":
+                    sonuc = sayi1 + sayi2;
+                    break;
+                case "-":
+                    sonuc = sayi1 - sayi2;
+                    break;
+                case "x":
+                    sonuc = sayi1 * sayi2;
+                    break;
+                case "/":
+                    sonuc = sayi1 / sayi2;
+                    break;
+                default:
+                    MessageBox.Show("İşlem seçmelisiniz!", "Hata");
+                    break;
+            }
+
+            textBox1.Text = sonuc.ToString();
         }
     }
 }
