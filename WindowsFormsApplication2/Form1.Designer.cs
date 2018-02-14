6842c1a860aee0bc4ecc180b1fd2307b2faaea4e
UsaAbd <ausan15@posta.pau.edu.tr>
2018-02-14 13:53:27 +0300

    tasarım eklendi

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,267 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonYedi = new System.Windows.Forms.Button();
+            this.buttonDort = new System.Windows.Forms.Button();
+            this.buttonSıfır = new System.Windows.Forms.Button();
+            this.buttonSekiz = new System.Windows.Forms.Button();
+            this.buttonBes = new System.Windows.Forms.Button();
+            this.buttonDokuz = new System.Windows.Forms.Button();
+            this.buttonİki = new System.Windows.Forms.Button();
+            this.buttonAltı = new System.Windows.Forms.Button();
+            this.buttonUc = new System.Windows.Forms.Button();
+            this.buttonVirgul = new System.Windows.Forms.Button();
+            this.buttonAC = new System.Windows.Forms.Button();
+            this.buttonBir = new System.Windows.Forms.Button();
+            this.buttonBolme = new System.Windows.Forms.Button();
+            this.buttonCarpma = new System.Windows.Forms.Button();
+            this.buttonCıkarma = new System.Windows.Forms.Button();
+            this.buttonToplama = new System.Windows.Forms.Button();
+            this.buttonEsittir = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonYedi
+            // 
+            this.buttonYedi.Location = new System.Drawing.Point(238, 277);
+            this.buttonYedi.Name = "buttonYedi";
+            this.buttonYedi.Size = new System.Drawing.Size(77, 51);
+            this.buttonYedi.TabIndex = 0;
+            this.buttonYedi.Text = "7";
+            this.buttonYedi.UseVisualStyleBackColor = true;
+            this.buttonYedi.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // buttonDort
+            // 
+            this.buttonDort.Location = new System.Drawing.Point(238, 334);
+            this.buttonDort.Name = "buttonDort";
+            this.buttonDort.Size = new System.Drawing.Size(77, 51);
+            this.buttonDort.TabIndex = 1;
+            this.buttonDort.Text = "4";
+            this.buttonDort.UseVisualStyleBackColor = true;
+            // 
+            // buttonSıfır
+            // 
+            this.buttonSıfır.Location = new System.Drawing.Point(238, 448);
+            this.buttonSıfır.Name = "buttonSıfır";
+            this.buttonSıfır.Size = new System.Drawing.Size(160, 51);
+            this.buttonSıfır.TabIndex = 3;
+            this.buttonSıfır.Text = "0";
+            this.buttonSıfır.UseVisualStyleBackColor = true;
+            // 
+            // buttonSekiz
+            // 
+            this.buttonSekiz.Location = new System.Drawing.Point(321, 277);
+            this.buttonSekiz.Name = "buttonSekiz";
+            this.buttonSekiz.Size = new System.Drawing.Size(77, 51);
+            this.buttonSekiz.TabIndex = 4;
+            this.buttonSekiz.Text = "8";
+            this.buttonSekiz.UseVisualStyleBackColor = true;
+            // 
+            // buttonBes
+            // 
+            this.buttonBes.Location = new System.Drawing.Point(321, 334);
+            this.buttonBes.Name = "buttonBes";
+            this.buttonBes.Size = new System.Drawing.Size(77, 51);
+            this.buttonBes.TabIndex = 5;
+            this.buttonBes.Text = "5";
+            this.buttonBes.UseVisualStyleBackColor = true;
+            // 
+            // buttonDokuz
+            // 
+            this.buttonDokuz.Location = new System.Drawing.Point(404, 277);
+            this.buttonDokuz.Name = "buttonDokuz";
+            this.buttonDokuz.Size = new System.Drawing.Size(77, 51);
+            this.buttonDokuz.TabIndex = 6;
+            this.buttonDokuz.Text = "9";
+            this.buttonDokuz.UseVisualStyleBackColor = true;
+            this.buttonDokuz.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // buttonİki
+            // 
+            this.buttonİki.Location = new System.Drawing.Point(321, 391);
+            this.buttonİki.Name = "buttonİki";
+            this.buttonİki.Size = new System.Drawing.Size(77, 51);
+            this.buttonİki.TabIndex = 7;
+            this.buttonİki.Text = "2";
+            this.buttonİki.UseVisualStyleBackColor = true;
+            // 
+            // buttonAltı
+            // 
+            this.buttonAltı.Location = new System.Drawing.Point(404, 334);
+            this.buttonAltı.Name = "buttonAltı";
+            this.buttonAltı.Size = new System.Drawing.Size(77, 51);
+            this.buttonAltı.TabIndex = 8;
+            this.buttonAltı.Text = "6";
+            this.buttonAltı.UseVisualStyleBackColor = true;
+            // 
+            // buttonUc
+            // 
+            this.buttonUc.Location = new System.Drawing.Point(404, 391);
+            this.buttonUc.Name = "buttonUc";
+            this.buttonUc.Size = new System.Drawing.Size(77, 51);
+            this.buttonUc.TabIndex = 9;
+            this.buttonUc.Text = "3";
+            this.buttonUc.UseVisualStyleBackColor = true;
+            // 
+            // buttonVirgul
+            // 
+            this.buttonVirgul.Location = new System.Drawing.Point(404, 448);
+            this.buttonVirgul.Name = "buttonVirgul";
+            this.buttonVirgul.Size = new System.Drawing.Size(77, 51);
+            this.buttonVirgul.TabIndex = 10;
+            this.buttonVirgul.Text = ",";
+            this.buttonVirgul.UseVisualStyleBackColor = true;
+            // 
+            // buttonAC
+            // 
+            this.buttonAC.Location = new System.Drawing.Point(238, 220);
+            this.buttonAC.Name = "buttonAC";
+            this.buttonAC.Size = new System.Drawing.Size(243, 51);
+            this.buttonAC.TabIndex = 11;
+            this.buttonAC.Text = "AC";
+            this.buttonAC.UseVisualStyleBackColor = true;
+            // 
+            // buttonBir
+            // 
+            this.buttonBir.Location = new System.Drawing.Point(238, 391);
+            this.buttonBir.Name = "buttonBir";
+            this.buttonBir.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.buttonBir.Size = new System.Drawing.Size(77, 51);
+            this.buttonBir.TabIndex = 2;
+            this.buttonBir.Text = "1";
+            this.buttonBir.UseVisualStyleBackColor = true;
+            // 
+            // buttonBolme
+            // 
+            this.buttonBolme.Location = new System.Drawing.Point(487, 220);
+            this.buttonBolme.Name = "buttonBolme";
+            this.buttonBolme.Size = new System.Drawing.Size(77, 51);
+            this.buttonBolme.TabIndex = 12;
+            this.buttonBolme.Text = "/";
+            this.buttonBolme.UseVisualStyleBackColor = true;
+            // 
+            // buttonCarpma
+            // 
+            this.buttonCarpma.Location = new System.Drawing.Point(487, 277);
+            this.buttonCarpma.Name = "buttonCarpma";
+            this.buttonCarpma.Size = new System.Drawing.Size(77, 51);
+            this.buttonCarpma.TabIndex = 13;
+            this.buttonCarpma.Text = "x";
+            this.buttonCarpma.UseVisualStyleBackColor = true;
+            // 
+            // buttonCıkarma
+            // 
+            this.buttonCıkarma.Location = new System.Drawing.Point(487, 334);
+            this.buttonCıkarma.Name = "buttonCıkarma";
+            this.buttonCıkarma.Size = new System.Drawing.Size(77, 51);
+            this.buttonCıkarma.TabIndex = 14;
+            this.buttonCıkarma.Text = "-";
+            this.buttonCıkarma.UseVisualStyleBackColor = true;
+            // 
+            // buttonToplama
+            // 
+            this.buttonToplama.Location = new System.Drawing.Point(487, 391);
+            this.buttonToplama.Name = "buttonToplama";
+            this.buttonToplama.Size = new System.Drawing.Size(77, 51);
+            this.buttonToplama.TabIndex = 15;
+            this.buttonToplama.Text = "+";
+            this.buttonToplama.UseVisualStyleBackColor = true;
+            // 
+            // buttonEsittir
+            // 
+            this.buttonEsittir.Location = new System.Drawing.Point(487, 448);
+            this.buttonEsittir.Name = "buttonEsittir";
+            this.buttonEsittir.Size = new System.Drawing.Size(77, 51);
+            this.buttonEsittir.TabIndex = 16;
+            this.buttonEsittir.Text = "=";
+            this.buttonEsittir.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(238, 163);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(326, 51);
+            this.textBox1.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1027, 772);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonEsittir);
+            this.Controls.Add(this.buttonToplama);
+            this.Controls.Add(this.buttonCıkarma);
+            this.Controls.Add(this.buttonCarpma);
+            this.Controls.Add(this.buttonBolme);
+            this.Controls.Add(this.buttonAC);
+            this.Controls.Add(this.buttonVirgul);
+            this.Controls.Add(this.buttonUc);
+            this.Controls.Add(this.buttonAltı);
+            this.Controls.Add(this.buttonİki);
+            this.Controls.Add(this.buttonDokuz);
+            this.Controls.Add(this.buttonBes);
+            this.Controls.Add(this.buttonSekiz);
+            this.Controls.Add(this.buttonSıfır);
+            this.Controls.Add(this.buttonBir);
+            this.Controls.Add(this.buttonDort);
+            this.Controls.Add(this.buttonYedi);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonYedi;
+        private System.Windows.Forms.Button buttonDort;
+        private System.Windows.Forms.Button buttonSıfır;
+        private System.Windows.Forms.Button buttonSekiz;
+        private System.Windows.Forms.Button buttonBes;
+        private System.Windows.Forms.Button buttonDokuz;
+        private System.Windows.Forms.Button buttonİki;
+        private System.Windows.Forms.Button buttonAltı;
+        private System.Windows.Forms.Button buttonUc;
+        private System.Windows.Forms.Button buttonVirgul;
+        private System.Windows.Forms.Button buttonAC;
+        private System.Windows.Forms.Button buttonBir;
+        private System.Windows.Forms.Button buttonBolme;
+        private System.Windows.Forms.Button buttonCarpma;
+        private System.Windows.Forms.Button buttonCıkarma;
+        private System.Windows.Forms.Button buttonToplama;
+        private System.Windows.Forms.Button buttonEsittir;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
